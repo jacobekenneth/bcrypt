d0f73df92343682693cfded893e3fa03dc4038a2
jacobekenneth <catarman1124@gmail.com>
2025-09-22 09:58:00 +0800

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-const bcrypt = require('bcrypt');
+let bcrypt = require('bcryptjs');
 
 
 //END_ASYNC
